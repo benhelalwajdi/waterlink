676d94b86b9df17e12d43e03daf2c288d313dcb8
gom3a <wajdibenhelal52@live.fr>
2019-02-19 11:39:19 +0100

    add the EditAction , PublicViewAction , AddCartAction , AddMeterAction , AddInvoiceAction

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -10,4 +10,31 @@ class DefaultController extends Controller
     {
         return $this->render('AdminBundle:Default:index.html.twig');
     }
+
+    public function EditAction()
+    {
+        return $this->render('AdminBundle:Default:Edit.html.twig');
+    }
+
+    public function PublicViewAction()
+    {
+        return $this->render('AdminBundle:Default:PublicView.html.twig');
+    }
+
+    public function AddCartAction()
+    {
+        return $this->render('AdminBundle:Default:AddCartAction.html.twig');
+    }
+
+    public function AddMeterAction()
+    {
+        return $this->render('AdminBundle:Default:AddMeter.html.twig');
+    }
+
+    //consulter les facteurs a chaque compteur
+    public function InvoiceAction()
+    {
+        return $this->render('AdminBundle:Default:Invoice.html.twig');
+    }
+
 }
